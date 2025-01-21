3c51b9d4effff54f4e73bae7b4f01eebd3065071
Marcjanna <marcjanna.cebulska@gmail.com>
2025-01-21 14:07:48 +0100

    filtering data2

--- trying.py
+++ trying.py
@@ -30,7 +30,7 @@ letters = {
     "Z"
 }
 
-filtered_names=[]
+filtered_data=[]
 
 
 for letter in letters:
@@ -39,31 +39,23 @@ for letter in letters:
 
     for dictionary in data: 
         person = {}
-        if "ontology/networth" in dictionary and "ontology/education_label" in dictionary:
+        if "ontology/networth" in dictionary and "ontology/education_label" in dictionary and "title" in dictionary:
+            person["name"] = (dictionary["title"])
             person["networth"] = (dictionary["ontology/networth"])
             if type(person["networth"]) is list:
                 person["networth"] = person["networth"][0]
-            person["education"] = (dictionary["ontology/education_label"])
-            if "ontology/occupation_label" in dictionary:
-                person["occupation"] = (dictionary["ontology/occupation_label"])
-            person1={}
-            person2={}
-            if type(person["education"]) is list:
-                if(len(person["education"])) == 2:
-                    person1["networth"] = person["networth"][0]
-                    person1["education"] = person["education"][0]
-                    if "occupation" in person: 
-                        person1["occupation"] = person["occupation"]
-                    person2["networth"] = person["networth"][0]
-                    person2["education"] = person["education"][1]
-                    if "occupation" in person:
-                        person2["occupation"] = person2["occupation"]
-        filtered_names.append(person, person1, person2)
+            educations = (dictionary["ontology/education_label"])
+            if type(educations) is not list:
+                educations = [educations]
+            for education in educations:
+                person = person.copy()
+                person["education"] = education
+                filtered_data.append(person)
 
-with open("results.json", "w", encoding="utf-8") as file:
-    json.dump(filtered_names, file, indent=4, ensure_ascii=False)
+with open("results2.json", "w", encoding="utf-8") as file:
+    json.dump(filtered_data, file, indent=4, ensure_ascii=False)
 
-with open("results.csv", "w", encoding="utf-8", newline='') as file:
-    writer = csv.DictWriter(file, ["networth", "education"])
+with open("results2.csv", "w", encoding="utf-8", newline='') as file:
+    writer = csv.DictWriter(file, ["name", "networth", "education"])
     writer.writeheader()
-    writer.writerows(filtered_names)
+    writer.writerows(filtered_data)
