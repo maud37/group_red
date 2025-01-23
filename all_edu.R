acfe4230817434ab8329eff372619a0f02ed7239
maud <maudvdberg123@gmail.com>
2025-01-23 17:39:06 +0100

    complete r for compliled uni's

--- all_edu.R
+++ all_edu.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-all_edu <- read_csv('all.csv') |>
+all_edu <- read_csv('all_real.csv') |>
   group_by(education) |>
   summarise(education_count = n()) |>
     
@@ -42,10 +42,48 @@ all_edu <- read_csv('all.csv') |>
            `Columbia University School of General Studies` + `Columbia University School of Social Work`+ `Columbia University School of the Arts` + `Columbia_University_School_of_Library_Science` +
            `School of International and Public Affairs Columbia University` + `Teachers College Columbia University`
          ) |>
-  
+  mutate(Yale_total = `Yale College` +   `Yale Divinity School` + `Yale Graduate School of Arts and Sciences` +`Yale Law School` +  `Yale School of Architecture` +
+         `Yale School of Art` + `Yale School of Drama` + `Yale School of Forestry & Environmental Studies` + `Yale School of Management` + `Yale School of Medicine` + 
+         `Yale School of Music` + `Yale School of Public Health` + `Yale University` + `Yale_University_(PhD)`
+      ) |>
+  mutate(Chicago_total = `University of Chicago` + `University of Chicago Divinity School` + 
+         `University of Chicago Laboratory Schools` + `University of Chicago Law School`
+    ) |>
+  mutate(
+    `University of Edinburgh` + `University of Edinburgh Law School` + `University of Edinburgh Medical School`
+  ) |>
+  mutate(
+    `University of Glasgow` + `University of Glasgow Medical School` + `University of Glasgow School of Veterinary Medicine`
+  )|>
+  mutate( Maryland_total =
+    `University of Maryland Baltimore` + `University of Maryland Baltimore County` + `University of Maryland Eastern Shore` + 
+      `University of Maryland University College` + `University of Maryland School of Law` + `University of Maryland School of Dentistry` +
+      `University of Maryland College Park` + `University of Maryland College of Agriculture and Natural Resources`
+  ) |> 
+  mutate(Melbourne_total = 
+    `University of Melbourne` + `University of Melbourne Faculty of VCA and MCM` + `St Joseph's College Melbourne` + `Trinity College (University of Melbourne)` + `Queen's College (University of Melbourne)`
+  ) |>
+  mutate(Michigan_total = 
+        `University of Michigan College of Literature Science and the Arts` + `University of Michigan School of Public Health` + `University of Michigan School of Music Theatre & Dance` + `University of Michigan School of Education` + 
+        `University of Michigan School of Dentistry` + `University of Michigan Law School` + `University of Michigan Health System` + `University of Michigan College of Pharmacy` 
+  ) |>
+  mutate(Minnesota_total =
+    `University of Minnesota` + `University of Minnesota Law School` + `University of Minnesota Medical School`
+  ) |>
+  mutate(Virginia_total =
+           `University of Virginia` + `University of Virginia Darden School of Business` + `University of Virginia School of Law` + 
+         `University of Virginia School of Medicine` + `University of Virginia School of Nursing`
+    ) |>
+  mutate(Pennsylvania_total =
+           `Pennsylvania State University` + `Pennsylvania State University - Dickinson Law`
+    ) |>
+  mutate(Pennsylvania_Ivy =
+        `University of Pennsylvania` + `University of Pennsylvania Economics Department` + `University of Pennsylvania Law School` + `University of Pennsylvania School of Dental Medicine` +
+         `University of Pennsylvania School of Engineering and Applied Science` + `University of Pennsylvania School of Nursing` + `Perelman School of Medicine at the University of Pennsylvania`
+  ) |> 
   pivot_longer(cols = everything()) |>
   rename(education = name, education_count = value) |>
-  filter(education_count > 100)
+  filter(education_count > 249)
 
 print(all_edu)
 
