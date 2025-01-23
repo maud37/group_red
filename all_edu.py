49b9c79f1eee2a43ded0957a10391fd530c74d0d
maud <maudvdberg123@gmail.com>
2025-01-23 16:22:51 +0100

    all edu + networth

--- all_edu.py
+++ all_edu.py
@@ -42,11 +42,15 @@ for letter in letters:
 
     for dictionary in data: 
         person = {}
-        if "title" in dictionary and ("ontology/education_label" in dictionary or "ontology/almaMater_label" in dictionary):
+        if "ontology/networth" in dictionary and "title" in dictionary and\
+        ("ontology/education_label" in dictionary or "ontology/almaMater_label" in dictionary):
             person["name"] = (dictionary["title"]).replace("_", " ")
             person["name"] = parentheses.sub("", person["name"])
+            person["networth"] = (dictionary["ontology/networth"])
+            if type(person["networth"]) is list:
+                person["networth"] = person["networth"][0]
             # if "ontology/parent" in dictionary:
-            #     person["parent1"] = (dictionary["ontology/parent_label"])
+                # person["parent1"] = (dictionary["ontology/parent_label"])
                 # if type(person["parent1"]) is list:
                 #     person["parent2"] = person["parent1"][1]
                 #     person["parent1"] = person["parent1"][0]
@@ -59,7 +63,7 @@ for letter in letters:
                 for education in educations:
                     person = person.copy()
                     person["education"] = education
-                    filtered_data.append(person)
+                filtered_data.append(person)
             if "ontology/almaMater_label" in dictionary:
                 alma_maters = (dictionary["ontology/almaMater_label"])
                 if type(alma_maters) is not list:
@@ -67,35 +71,19 @@ for letter in letters:
                 for alma_mater in alma_maters:
                     person = person.copy()
                     person["education"] = alma_mater
-                    filtered_data.append(person)
+                filtered_data.append(person)
 
 
-print(len(filtered_data))
+# print(len(filtered_data)
 
 
 with open("all.json", "w", encoding="utf-8") as file:
     json.dump(filtered_data, file, indent=4, ensure_ascii=False)
 
 with open("all.csv", "w", encoding="utf-8", newline='') as file:
-    writer = csv.DictWriter(file, ["name", "education"])
+    writer = csv.DictWriter(file, ["name", "networth", "education"])
     writer.writeheader()
     writer.writerows(filtered_data)
 
 
 
-# for person in filtered_data:
-#     wiki_parent = {}
-    
-
-
-
-
-# print(filtered_data)
-
-# remove_doubles = set()
-
-# for person in filtered_data:
-#     remove_doubles.add(person["education"])
-
-# print(remove_doubles)
-# print(len(remove_doubles))
